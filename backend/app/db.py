95c80149f6793e16b5689856aefe8cfe84084b79
DavidIfebueme <davidifebueme@gmail.com>
2025-06-16 07:45:20 +0100

    fix: db fixes

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,10 +1,18 @@
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 from app.config import settings
 
-engine = create_async_engine(settings.DB_URL, echo=True, future=True)
-AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+async_db_url = settings.DB_URL.replace('postgresql:', 'postgresql+asyncpg:')
 
-async def get_db():
+engine = create_async_engine(async_db_url, echo=True, future=True)
+
+AsyncSessionLocal = sessionmaker(
+    engine, class_=AsyncSession, expire_on_commit=False
+)
+
+async def get_db() -> AsyncSession:
     async with AsyncSessionLocal() as session:
-        yield session
+        try:
+            yield session
+        finally:
+            await session.close()
